7ffe4fa2ce623f2c23c406132ef0bc3088330b6d
Laureen Dumas <laureen262@hotmail.fr>
2019-11-18 19:10:58 +0100

    quete slug done

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -18,4 +18,21 @@ class WildController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
+    /**
+     * @Route("/wild/show/{slug}",
+     *     requirements={"slug"="^[a-z0-9]+(?:-[a-z0-9]+)*$"},
+     *     name="wild_show")
+     *     @param string $slug
+     *     @return Response
+     */
+    public function show(string $slug = ''): Response
+    {
+        if ($slug === '') {
+            $title = 'Aucune série sélectionnée, veuillez choisir une série';
+        } else {
+            $title = ucwords(str_replace('-', ' ', $slug));
+        }
+        return $this->render('wild/show.html.twig', ['title' => $title]);
+    }
 }
