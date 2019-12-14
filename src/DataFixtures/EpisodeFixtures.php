0093f5f189ecea6576b878ed255d8dbb93c45a85
Laureen Dumas <laureen262@hotmail.fr>
2019-12-14 21:41:41 +0100

    quete 15 done

---
+++ src/DataFixtures/EpisodeFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Episode;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class EpisodeFixtures extends Fixture implements DependentFixtureInterface
+{
+    /**
+     * Load data fixtures with the passed EntityManager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 50; $i++){
+            $episode = new Episode();
+            $episode->setTitle($faker->text(20));
+            $episode->setNumber($faker->randomDigit);
+            $episode->setSynopsis($faker->text(300));
+            $number = rand(0, 19);
+            $episode->setSeason($this->getReference('season_' . $number));
+            $manager->persist($episode);
+        }
+        $manager->flush();
+    }
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [SeasonFixtures::class];
+    }
+
+
+}
---
+++ src/DataFixtures/SeasonFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+use App\Entity\Season;
+use App\Entity\Program;
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class SeasonFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+        for ($i = 0; $i < 20; $i++) {
+            $season = new Season();
+            $season->setNumber($faker->unique()->numberBetween(1,20));
+            $season->setYear($faker->year);
+            $season->setDescription($faker->sentence(40));
+            $number = rand(0,4);
+            $season->setProgram($this->getReference('program_' . $number));
+            $manager->persist($season);
+            $this->addReference('season_' . $i, $season);
+        }
+        $manager->flush();
+    }
+
+    /**
+     * This method must return an array of fixtures classes
+     * on which the implementing class depends on
+     *
+     * @return array
+     */
+    public function getDependencies()
+    {
+        return [ProgramFixtures::class];
+    }
+}
